d505117ac4cb567648e8c5c22f9e99562ede751c
Ashley Baer <37188478+ashleybaer@users.noreply.github.com>
2018-03-08 14:48:23 -0500

    Fix matrix multiplication in FilterRLS (#3).

--- padasip/filters/rls.py
+++ padasip/filters/rls.py
@@ -217,7 +217,8 @@ class FilterRLS(AdaptiveFilter):
         """
         y = np.dot(self.w, x)
         e = d - y
-        R1 = np.dot(np.dot(np.dot(self.R,x),x.T),self.R)
+        x_2d = x[:, np.newaxis]
+        R1 = np.dot(np.dot(np.dot(self.R,x_2d),x_2d.T),self.R)
         R2 = self.mu + np.dot(np.dot(x,self.R),x.T)
         self.R = 1/self.mu * (self.R - R1/R2)
         dw = np.dot(self.R, x.T) * e
@@ -265,7 +266,8 @@ class FilterRLS(AdaptiveFilter):
             self.w_history[k,:] = self.w
             y[k] = np.dot(self.w, x[k])
             e[k] = d[k] - y[k]
-            R1 = np.dot(np.dot(np.dot(self.R,x[k]),x[k].T),self.R)
+            xk_2d = x[k][:, np.newaxis]
+            R1 = np.dot(np.dot(np.dot(self.R,xk_2d),xk_2d.T),self.R)
             R2 = self.mu + np.dot(np.dot(x[k],self.R),x[k].T)
             self.R = 1/self.mu * (self.R - R1/R2)
             dw = np.dot(self.R, x[k].T) * e[k]
